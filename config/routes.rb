a0e7a3c9c7068c213eda511ef02323c7e717072a
gengotsukai <benioni2005@yahoo.co.jp>
2020-04-01 18:21:24 +0900

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :users do
+  	resource :deliveries, only: [:index, :edit, :update, :destroy, :create]
+  end
+  resources :admins, only:
+  resources :products do
+  	resource :cart_items, only: [:create, :update, :destroy, :index]
+  	resource :genres, only: [:index, :edit, :update, :destroy, :create]
+  end
+  resources :orders, only:
+  resources :order_items, only:
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'user/thanks' => 'orders#thanks'
 end
