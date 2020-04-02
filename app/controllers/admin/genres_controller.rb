1a06ee1cd7df3ce265ceee386d29539fcc884a6c
keiba804 <mottuso0017@gmail.com>
2020-04-02 08:23:25 +0000

    [edit] controllers/admin

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,12 @@
 class Admin::GenresController < ApplicationController
+	def index
+	end
+	def edit
+	end
+	def update
+	end
+	def destroy
+	end
+	def create
+	end
 end
--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,16 @@
 class Admin::ProductsController < ApplicationController
+	def index
+	end
+	def edit
+	end
+	def update
+	end
+	def destroy
+	end
+	def create
+	end
+	def new
+	end
+	def show
+	end
 end
