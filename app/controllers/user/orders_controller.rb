ad3c342b4816548801608119a09e8a1ddb9eb201
nao626 <rock6770@gmail.com>
2020-04-02 08:25:33 +0000

    [Add]controllers/user

--- app/controllers/user/orders_controller.rb
+++ app/controllers/user/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def confirm
+  end
+
+  def thanks
+  end
+
+  def show
+  end
+  
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  
+  def top
+  end
+  
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def confirm
+  end
+  
 end
