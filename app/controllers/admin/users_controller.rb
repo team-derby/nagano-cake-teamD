63c0699a0954c1480c2f4051210ce185a5ffa934
nao626 <rock6770@gmail.com>
2020-04-05 04:15:46 +0000

    [Add]controllers,views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,12 +1,27 @@
 class Admin::UsersController < ApplicationController
+
 	def index
+		@users = User.all
 	end
+
 	def edit
+		@user = User.find(params[:id])
 	end
+
 	def update
+		@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user)
+  	else
+  		render :edit
+  	end
 	end
+
 	def destroy
 	end
+
 	def show
+		@user = User.find(params[:id])
 	end
+
 end
--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,15 +1,23 @@
 class User::UsersController < ApplicationController
-  git
+
   def top
   end
   
   def show
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+  	if @user.update(user_params)
+  		redirect_to user_path(@user)
+  	else
+  		render :edit
+  	end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def destroy
