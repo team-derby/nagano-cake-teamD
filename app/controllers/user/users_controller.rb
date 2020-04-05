6edc0e6d25d24d19beca026ddde1271c540dc22d
gengotsukai <benioni2005@yahoo.co.jp>
2020-04-05 14:08:52 +0900

    [Add] login and logout functions

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -7,6 +7,11 @@ class User::UsersController < ApplicationController
   end
 
   def update
+      if current_user.update(user_params)
+         redirect_to root_path #更新できた場合はTOP画面へ
+      else
+         render :edit #更新できない場合は編集画面に戻る
+      end
   end
 
   def edit
@@ -17,5 +22,17 @@ class User::UsersController < ApplicationController
 
   def confirm
   end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :introduction, :profile_image)
+  end
+
+  #url直接防止　メソッドを自己定義してbefore_actionで発動。
+   def baria_user
+    unless params[:id].to_i == current_user.id
+      redirect_to user_path(current_user)
+    end
+   end
 
 end
