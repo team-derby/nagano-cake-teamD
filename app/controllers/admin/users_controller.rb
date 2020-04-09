33d2027972ee49ead3c8561778db0fc32aaaefd6
nao626 <rock6770@gmail.com>
2020-04-09 04:40:29 +0000

    [FIX]user_admin,controller,model,view

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -12,7 +12,7 @@ class Admin::UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      redirect_to user_path(@user)
+      redirect_to admin_user_path(@user)
     else
       render :edit
     end
@@ -23,6 +23,11 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name_kanji, :last_name_kanji, :first_name_kana, :last_name_kana, :post_number, :address, :phone_number, :email)
   end
 
 end
