62fdc6089748df88e3045a8e9891914d6257e6a1
keiba804 <mottuso0017@gmail.com>
2020-04-12 08:35:10 +0000

    [edit]admins top

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -3,6 +3,9 @@ class Admin::AdminsController < ApplicationController
   before_action :authenticate_admin!
 
 	def top
+		@today = Date.today
+		@orders = Order.all
+		@today_count = @orders.where(created_at: @today.all_day).count
 	end
 
 end
